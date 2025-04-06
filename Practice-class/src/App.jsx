6400ca0b8810459711352d7435a61e66d1e08506
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-06 14:33:38 +0530

    update

--- Practice-class/src/App.jsx
+++ Practice-class/src/App.jsx
@@ -1,10 +1,13 @@
+import Footer from "./footer";
+import Header from "./header";
 
 
 const App=()=>{
   return(
     <>
        <h1>Welcome To React Class</h1>
-       
+       < Header/>
+       < Footer/>
     </>
   )
 }
