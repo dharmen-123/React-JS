233df11815d73d2f6508f89914a79729a21a5874
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-08 16:36:13 +0530

    commit

--- class5th/src/App.jsx
+++ class5th/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App=()=>{
+  const [cnt , setCnt]  = useState("0")
+  const myincre=()=>{
+    setCnt(cnt+1)
+  }
+  const mydecre=()=>{
+    if (cnt<1){
+      alert("Counter not less than 1")
+    }
+    else{
+      setCnt(cnt-1)
+    }
+  }
 
-  return (
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div id="Counterbox">
+     <h1>Counter App:</h1>
+     <h1>Count:{cnt}</h1>
+     <button onClick={myincre}>Increment</button>
+     <button onClick={mydecre}>Dcrement</button> <br/><br />
+     <button onClick={()=>{setCnt(0)}} id="reset">Reset</button>
+     </div>
     </>
   )
 }
 
-export default App
+export default App;
