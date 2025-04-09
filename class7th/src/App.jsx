99940ecf29ab8f64f5750feaed548a77d3395ede
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-09 17:04:04 +0530

    update

--- class7th/src/App.jsx
+++ class7th/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+const App=()=>{
+   const [input , setinp]=useState({})
+
+   const handleinp=(key)=>{
+      let  name=key.target.name;
+      let  value=key.target.value;
+     setinp(values=>({...values,[name]:value}))
+     console.log(input);
+   }
+  return(
+   <>
+   <h1>Application Form</h1>
+   Enter rollno. :<input type="text" name="rollno." onChange={handleinp} />
+   <br/><br/>
+   Enter name :<input type="text" name="name" onChange={handleinp} />
+   <br/><br/>
+   Enter fees:<input type="text" name="fees" onChange={handleinp} />
+   <br/><br/>
+   Enter city:<input type="text" name="city" onChange={handleinp} />
+   <br/><br/>
+   <button>Save</button>
+   </>
   )
 }
-
-export default App
+export default App;
