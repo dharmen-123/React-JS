b6dad595e8325bbdbb7de64cb7cbee27942de1d8
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-10 09:39:00 +0530

    commit

--- class7th/src/App.jsx
+++ class7th/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 
 const App=()=>{
-   const [input , setinp]=useState({})
+   const [input , setinp] = useState({})
 
    const handleinp=(key)=>{
       let  name=key.target.name;
