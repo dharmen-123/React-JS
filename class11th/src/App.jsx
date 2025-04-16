943284b8476318c935134f5bc5d364de16d49016
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-16 17:06:31 +0530

    update

--- class11th/src/App.jsx
+++ class11th/src/App.jsx
@@ -1,1 +1,17 @@
+import {useState, useContext } from "react";
+import Bhopal from "./Bhopal";
+const myContext=createContext();
+const App=()=>{
+  const [name , setname]= useState("Dharmendra")
+  return(
+    <>
+    <h1>Welcome!!!</h1>
+  <myContext.Provider value={{name,setname}}>
+    <Bhopal/>
+  </myContext.Provider>
+    </>
+  )
+}
 
+export default App;
+export {myContext};
---
+++ class11th/src/Bhopal.jsx
@@ -0,0 +1,12 @@
+import { myContext } from "./App";
+import { useContext } from "react";
+
+const Bhopal=()=>{
+    const {name}= useContext(myContext)
+    return(
+        <>
+        <h1>Welcome {name}</h1>
+        </>
+    )
+}
+export default Bhopal;
