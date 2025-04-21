af5e2282559324aea60f1a08dc8ce9a52c284e5a
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-21 15:46:21 +0530

    changes

--- class9th/src/Component/Topbar.jsx
+++ class9th/src/Component/Topbar.jsx
@@ -1,8 +1,8 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
 import { Link } from "react-router-dom";
+
 const Topbar=()=>{
 
     return(
@@ -22,6 +22,7 @@ const Topbar=()=>{
         </Container>
       </Navbar>
       </div>
+
         </>
     )
 }
--- class9th/src/Pages/Insert.jsx
+++ class9th/src/Pages/Insert.jsx
@@ -67,4 +67,5 @@ const Insert=()=>{
     )
 }
 
+
 export default Insert;
