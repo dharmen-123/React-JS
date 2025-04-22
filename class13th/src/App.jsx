4798c27e3846408271138b767466a01ea9eaddec
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-22 17:20:49 +0530

    changes

--- class13th/src/App.jsx
+++ class13th/src/App.jsx
@@ -1,1 +1,40 @@
+import { useRef } from "react";
+import Render from "./Render";
 
+const App=()=>{
+  const myRef= useRef("");
+  const myFun1=()=>{
+        myRef.current.style.border="2px solid black";
+        myRef.current.style.width="400px";
+        myRef.current.style.height="300px";
+        myRef.current.style.textAlign="center";
+        myRef.current.style.alignItem="center";
+        myRef.current.style.margin="auto";
+        myRef.current.style.alignContent="center";
+        myRef.current.innerHTML="We learn React JS From Cybrom";
+  }
+  const myFun2=()=>{
+       myRef.current.style.borderRadius="50%";
+       myRef.current.style.backgroundColor="yellow";
+
+  }
+  const myFun3=()=>{
+    myRef.current.style.fontSize="40px";
+    myRef.current.style.color="green";
+
+}
+
+  return(
+    <>
+     <button onClick={myFun1}>Click here</button>
+     <button onClick={myFun2}>Click here</button>
+     <button onClick={myFun3}>Click here</button>
+      <div ref={myRef}>
+        Hello Friends;
+      </div>
+      <Render/>
+    </>
+  )
+}
+
+export default App;
---
+++ class13th/src/Render.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useRef, useState } from "react";
+const Render=()=>{
+   const [name , setName]=useState("");
+   const cntRef=useRef(0);
+  useEffect(()=>{
+    cntRef.current=cntRef.current+1;
+  })
+
+
+    return(
+        <>
+        Enter Your name: <input type="text"  value={name}  
+        onChange={(e)=>{setName(e.target.value)}}/>
+        <h1>My render Count :{cntRef}</h1>
+        </>
+    )
+}
+
+
+export default Render;
