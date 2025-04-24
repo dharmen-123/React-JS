9044bfeb28d0aeba71875286611fa66bf2146e93
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-24 17:25:27 +0530

    chnages

--- usememoclass14/src/Number.jsx
+++ usememoclass14/src/Number.jsx
@@ -1,9 +1,14 @@
-
+import { useState, useMemo } from "react";
 
 const Number=()=>{
+    const [mynum , setNum]=usestate("")
+    const [myname , setName]=usestate("")
+
     return(
         <>
         <h1>Number</h1>
+        <input type="number" value={mynum} onChange={(e)=>{e.target.value}}/>
+        <h1>Multiplication :{}</h1>
         </>
     )
 }
