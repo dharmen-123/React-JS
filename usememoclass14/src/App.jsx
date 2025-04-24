57be6d5d62f8e15745d51e4dac5d9758a0ccffb4
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-24 17:15:48 +0530

    commit

--- usememoclass14/src/App.jsx
+++ usememoclass14/src/App.jsx
@@ -1,2 +1,24 @@
+import { useState , useMemo } from "react";
 
+const App=()=>{
 
+  const [add ,setadd]=useState(0);
+  const [sub ,setsub]=useState(100);
+  const myMulti = useMemo(()=>{
+    console.log("#######");
+    return add*2;
+  },[add]);
+  return(
+    <>
+    <h1>Addition : {add}</h1>
+    <button onMouseEnter={()=>{setadd(add+1)}}>Addition</button>
+    <h1>Subtraction : {sub}</h1>
+    <button onClick={()=>{setsub(sub-1)}}>Subtraction</button>
+    <hr />
+    <h1>Multiplication : {myMulti}</h1>
+    
+    </>
+  )
+}
+
+export default App;
