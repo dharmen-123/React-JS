7ad44dfd3d18ecc6ad404cbd64684e1fa906ae55
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-25 17:18:31 +0530

    commit

--- contextapi/src/App.jsx
+++ contextapi/src/App.jsx
@@ -1,3 +1,17 @@
+import { useRef } from "react";
 
+const App=()=>{
+  const myRef = useRef();
+   const myShow=()=>{
+    myRef.current.innerHTML="I am Dharmendra";
+    myRef.current.style.color="red";
+   }
+  return(
+    <>
+     <h1 ref={myRef}>Welcome to Cybrom</h1>
+     <button onClick={myShow}>Click here</button>
+    </>
+  )
+}
 
-
+export default App;
