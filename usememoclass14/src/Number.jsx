58c9d2792fdd69e0d76622c2d39126a830636f43
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-25 16:32:42 +0530

    changes

--- usememoclass14/src/Number.jsx
+++ usememoclass14/src/Number.jsx
@@ -4,11 +4,16 @@ const Number=()=>{
     const [mynum , setNum]=usestate("")
     const [myname , setName]=usestate("")
 
+       function myMulti(){
+        for(var i=0; i<10000000;i++){}
+        return mynum*2;
+       }
     return(
         <>
         <h1>Number</h1>
-        <input type="number" value={mynum} onChange={(e)=>{e.target.value}}/>
-        <h1>Multiplication :{}</h1>
+         Select No :<input type="number" value={mynum} onChange={(e)=>{setNum(e.target.value)}}/>
+         Enter Name :<input type="text" value={myname} onChange={(e)=>{setName(e.target.value)}}/>
+        <h1>Multiplication :{myMulti()}</h1>
         </>
     )
 }
