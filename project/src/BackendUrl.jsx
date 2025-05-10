4367543b576144036724ec5e9431615517f1a82d
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-05-10 17:39:34 +0530

    search

---
+++ project/src/BackendUrl.jsx
@@ -0,0 +1,5 @@
+
+
+const BackendUrl="http://localhost:3000/Fashion";
+
+export default BackendUrl;
--- project/src/Pages/Home.jsx
+++ project/src/Pages/Home.jsx
@@ -1,28 +1,70 @@
 import Carousel from 'react-bootstrap/Carousel';
-import weblogo from "../Image/logo.jpg";
 import img1 from "../Image/slider1.png";
+import img2 from "../Image/slider2.png";
+import img3 from "../Image/slider3.png";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import BackendUrl from '../BackendUrl';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+// import { addToCart } from '../cartSlice';
+// import { useDispatch } from 'react-redux';
 const Home=()=>{
+ const [mydata, setMydata] = useState([]);
+//     // const dispatch= useDispatch();
+   
+    const loadData=async()=>{
+      const response = await axios.get(BackendUrl);
+      console.log(response.data);
+      setMydata(response.data);
+    }
+
+    useEffect(()=>{
+        loadData();
+    }, [])
+
+    const ans= mydata.map((key)=>{
+        return(
+            <>
+                 <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={key.image} />
+      <Card.Body>
+        <Card.Title>{key.name}</Card.Title>
+        <Card.Text>
+          {key.description}
+        </Card.Text>
+        <h3> Price : {key.price}</h3>
+        <Button variant="primary"
+        // onClick={()=>{dispatch(addToCart({id:key.id, name:key.name, description:key.description,category:key.category, image:key.image, qnty:1, price:key.price }))}}
+        >Add To Cart</Button>
+      </Card.Body>
+    </Card>
+            
+            </>
+        )
+    })
+
+
     return(
         <>
-        
           <Carousel>
-      <Carousel.Item interval={500}>
-        <img src={img1} width="100%" height="200px"/>
+      <Carousel.Item interval={700}>
+        <img src={img1} width="100%" height="600px"/>
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={500}>
-       <img src={img1} />
+      <Carousel.Item interval={700}>
+       <img src={img2}  width="100%" height="600px"/>
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={500}>
+      <Carousel.Item interval={700}>
 
-        <img src={img1} />
+        <img src={img3}  width="100%" height="600px"/>
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
@@ -31,6 +73,10 @@ const Home=()=>{
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel> 
+        <br/>
+             <div className='homeProduct'>
+          {ans}
+          </div>
           
         </>
     )
