7be459cd89ade3967d12b20e23472c82c8242b06
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-05-22 19:13:24 +0530

    Crud

--- class9th/src/Component/Footer.jsx
+++ class9th/src/Component/Footer.jsx
@@ -4,6 +4,7 @@ const  Footer=()=>{
     return(
         <>
           <div id="footer">
+            
            Copyright @  www.Aluminidata.com  All  rights  reserverd  2025
           </div>
         </>
--- class9th/src/main.jsx
+++ class9th/src/main.jsx
@@ -4,8 +4,10 @@ import App from './App.jsx'
 
 import "./CSS/style.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 createRoot(document.getElementById('root')).render(
      
+
       <App />
 
 )
