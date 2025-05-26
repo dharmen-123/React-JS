055d4223d90268bfaa0adbaf383749bf1eddf8df
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-05-26 15:31:21 +0530

    changes

--- vivatask/src/Traffic.jsx
+++ vivatask/src/Traffic.jsx
@@ -3,25 +3,30 @@ import { useState , useEffect} from "react";
 const TrafficLight=()=>{
 
     const [light , setlight]=useState('red');    
-    
+   const [run , setrun]=useState(true);
     useEffect(()=>{
+        if(!run) return;
+
         let time;
         switch(light){
             case 'red':
-                time=setTimeout(()=>setlight('red'),4000);
+                time = setTimeout(()=>setlight('red'),4000);
             case 'yellow':
-                time=setTimeout(()=>setlight('yellow'),500);
+                time = setTimeout(()=>setlight('yellow'),500);
             case 'green':
-                time=setTimeout(()=>setlight('green'),3000);
+                time = setTimeout(()=>setlight('green'),3000);
             default:
             break
         }
-    },[light]);
+    },[light, run]);
 
+ const Stop=()=>{
+      setrun(false)
+ }
    const changecolor=(status)=>(
-    light==status? status:'grey')
+         light==status? status:'grey')
     
-return(
+  return(
         <>
        <div id="vertical">
          <div className="circle" style={{backgroundColor:changecolor('red')}} ></div>
@@ -33,6 +38,7 @@ return(
          <div className="circle" style={{backgroundColor: changecolor('yellow')}} ></div>
          <div className="circle" style={{backgroundColor: changecolor('green')}} ></div>    
       </div>
+      <button onClick={Stop} style={{marginLeft:"50%", padding:"12px"}}><h1>stop</h1></button>
         </>
     )
 }
